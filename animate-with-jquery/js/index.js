400bdd42064ec480b549237e176e3b59ed05dca8
Dpappy <fjordbenson@gmail.com>
2016-03-17 17:17:11 -0700

    edited 404 page

---
+++ animate-with-jquery/js/index.js
@@ -0,0 +1,8 @@
+$('nav li').click(function(){
+  
+  $('nav li').animate({
+  'margin-top': '400px',
+    'opacity': '0'
+}, 1000)
+  
+});
--- letterfx/jquery-letterfx.js
+++ letterfx/jquery-letterfx.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function ($p) {
 
     "use strict";
 
